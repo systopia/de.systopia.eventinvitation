08ce7039c7d98d58555e033a6ae490a661c58e4f
B. Endres <endres@systopia.de>
2020-11-05 12:08:32 +0100

    update l10n

--- update_pot.sh
+++ update_pot.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # quick and dirty script to update the .pot file
+mv vendor ..
 l10n_tools="../civi_l10n_tools"
 ${l10n_tools}/bin/create-pot-files-extensions.sh de.systopia.eventinvitation ./ l10n
+mv ../vendor ./
