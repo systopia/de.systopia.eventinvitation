64aa0651d339ffb8450e81a4e42f052f0ce48da8
Sven Nowotnik <nowotnik@systopia.de>
2024-04-17 11:18:33 +0200

    altered value verification based on empty() to explicit verification for non-empty string and flag value set to 1

--- CRM/Eventinvitation/Queue/Runner/Job.php
+++ CRM/Eventinvitation/Queue/Runner/Job.php
@@ -150,8 +150,10 @@ abstract class CRM_Eventinvitation_Queue_Runner_Job
         $link = '';
         if (
             is_array($settings)
-            && ! empty($settings[CRM_Eventinvitation_Form_Settings::LINK_TARGET_IS_CUSTOM_FORM_NAME])
-            && ! empty($settings[CRM_Eventinvitation_Form_Settings::CUSTOM_LINK_TARGET_FORM_NAME])
+            && isset($settings[CRM_Eventinvitation_Form_Settings::LINK_TARGET_IS_CUSTOM_FORM_NAME])
+            && 1 == $settings[CRM_Eventinvitation_Form_Settings::LINK_TARGET_IS_CUSTOM_FORM_NAME]
+            && isset($settings[CRM_Eventinvitation_Form_Settings::CUSTOM_LINK_TARGET_FORM_NAME])
+            && '' !== $settings[CRM_Eventinvitation_Form_Settings::CUSTOM_LINK_TARGET_FORM_NAME]
         ) {
             // get the link
             $link = $settings[CRM_Eventinvitation_Form_Settings::CUSTOM_LINK_TARGET_FORM_NAME];
